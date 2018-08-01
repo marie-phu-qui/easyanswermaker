8a84ebaa440212d64fb7e4276e8bd3911bc173e4
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2018-08-01 15:50:06 +1200

    functionning answers

--- easyanswer.js
+++ easyanswer.js
@@ -2,6 +2,14 @@ let answers = ["YES", "NO", "Ask me later."];
 let button = $("input.button");
 let chosenAnswer = answers[Math.floor(Math.random() * answers.length)];
 
+//make former answer disappear when clicking the question input
+function clearScreen(){
+    if($("#answer").length>0){
+        $("#answer").fadeOut(2000);
+        $("#attention").fadeOut(2000);
+    }
+}
+
 //random answer
 function chooseAnswer(){
     let chosenAnswer = answers[Math.floor(Math.random() * answers.length)];
@@ -10,14 +18,17 @@ function chooseAnswer(){
     function displayAnswer(){
         if (chosenAnswer=="YES"){
             $("#answer").html("YES");
+            $("#answer").fadeIn("slow");
             return;
         }
         else if (chosenAnswer=="NO"){
-            $("#answer").html("NO");     
+            $("#answer").html("NO"); 
+            $("#answer").fadeIn("slow");
             return;
         }   
         else {
-            $("#answer").html("Later");
+            $("#answer").html("Ask me later.");
+            $("#answer").fadeIn("slow");
             return;
         }
         console.log(chosenAnswer);
@@ -29,18 +40,19 @@ function chooseAnswer(){
 //check if this is a proper question
 function checkQuestion(){
     if (document.getElementById("question").value.includes("?")){
-    console.log("This is a proper question");
     chooseAnswer();
     return;
     }
     else {
     // will appear big on screen with a fadeout
+    $("#attention").html("Ask me something with a '?'");
+    $("#attention").fadeIn("slow");
+
     console.log("Ask me something with a '?'");
+
     return;
     }
 }
-
-
 
 //gives answer
 function answer(){
